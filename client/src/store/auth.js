05b7f430d6035e4694da15d90d7c95686f6bcb6b
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-09 20:55:15 +0300

    get authenticated user

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -7,26 +7,49 @@ export default {
         authenticated: false,
         user: null
     },
+
     getters: {
-        authenticated(state) {
+        authenticated (state) {
             return state.authenticated
         },
 
         user (state) {
             return state.user
-        }
+        },
     },
-    mutations: {
 
-    },
-    actions: {
-        async signIn ({ commit }, credentials) {
-            await axios.get('/airlock/csrf-cookie')
-            await axios.post('/login', credentials)
+    mutations: {
+        SET_AUTHENTICATED (state, value) {
+            state.authenticated = value
         },
 
-        async me ({ commit }) {
-            
+        SET_USER (state, value) {
+            state.user = value
+        }
+    },
+
+    actions: {
+        async signIn ({ dispatch }, credentials) {
+            await axios.get('/airlock/csrf-cookie')
+            await axios.post('/login', credentials)
+
+            return dispatch('me')
+        },
+
+        async signOut ({ dispatch }) {
+            await axios.post('/logout')
+
+            return dispatch('me')
+        },
+
+        me ({ commit }) {
+            return axios.get('/api/user').then((response) => {
+                commit('SET_AUTHENTICATED', true)
+                commit('SET_USER', response.data)
+            }).catch(() => {
+                commit('SET_AUTHENTICATED', false)
+                commit('SET_USER', null)
+            })
         }
     }
 }
