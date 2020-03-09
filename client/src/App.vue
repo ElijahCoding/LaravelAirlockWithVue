6d0d29c8e6d702d2f371b7411aea514c352cefbe
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-09 19:09:10 +0300

    tailwind templates

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <AppNav />
     <router-view/>
   </div>
 </template>
+
+<script>
+  import AppNav from '@/components/global/AppNav'
+
+  export default {
+    components: {
+      AppNav
+    }
+  }
+</script>
+
+<style src="@/assets/styles/app.css" />
