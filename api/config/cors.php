336aae410b33ec5d7b03105cfcfd3f00ec02b595
ElijahCoding <cuiyeqing960904@gmail.com>
2020-03-09 17:50:44 +0300

    airlock , session, cors setup

--- api/config/cors.php
+++ api/config/cors.php
@@ -15,7 +15,13 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => [
+        'api/*',
+        'login',
+        'logout',
+        'airlock/csrf-cookie',
+        ''
+    ],
 
     'allowed_methods' => ['*'],
 
@@ -29,6 +35,6 @@ return [
 
     'max_age' => false,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
